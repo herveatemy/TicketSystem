140d84bcca92dfb4c21fed36289f9c93bc291534
herveatemy <herveatemy@yahoo.fr>
2018-01-14 14:21:17 +0100

    Mon premier commit après creation du fichier dans vs studio

---
+++ TicketSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicketSystem.Startup))]
+namespace TicketSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
